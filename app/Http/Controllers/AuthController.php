06d4aaf69ceb624e171c450a3366390e37fb1a2c
Felipe Schottz <felipe.schottz@memora.com.br>
2023-08-28 20:49:33 -0300

    Ajustar login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,31 +30,26 @@ class AuthController extends Controller
         }
     }
 
- 
-
-
     public function authenticateEleitor(Request $request)
     {
-        dd($request->all());
         // Obtenha o código SMS enviado para o telefone do eleitor
         $codigoSMS = $request->input('codigo_sms');
+        $codigoarmazenado = $request->session()->get('codigo_sms');
 
         // Verifique se o código SMS é válido (pode ser implementado de acordo com a sua lógica)
-        $codigoSMSValido = true; // Exemplo: considerando válido para fins de demonstração
+        $codigoSMSValido = $codigoarmazenado == $codigoSMS; // Exemplo: considerando válido para fins de demonstração
 
         if ($codigoSMSValido) {
-            $matricula = $request->input('matricula');
-            
+            $matricula = $request->session()->get('matricula');
         
             // Buscar o eleitor com base na matrícula
             $eleitor = Eleitor::where('matricula', $matricula)->first();
-            
         
             if ($eleitor) {
                 // Autenticar o eleitor
                 Auth::loginUsingId($eleitor->id);
         
-                return redirect()->route('exibir-eleicao')->with('success', 'Autenticação realizada com sucesso.');
+                return redirect()->route('exibir-eleicao', $eleitor->eleicao_id)->with('success', 'Autenticação realizada com sucesso.');
             } else {
                 return redirect()->back()->with('error', 'Eleitor não encontrado.');
             }
@@ -76,9 +71,10 @@ class AuthController extends Controller
 
             // Gerar um código SMS aleatório
             $codigoSMS = mt_rand(1000, 9999);
-
+            error_log($codigoSMS);
             // Salvar o código SMS na sessão para validação posterior
             $request->session()->put('codigo_sms', $codigoSMS);
+            $request->session()->put('matricula', $matricula);
 
             // Criar uma URL para o endpoint authenticateEleitor com o código SMS e a matrícula
             $url = URL::signedRoute('authenticate-eleitor', [
@@ -104,13 +100,10 @@ class AuthController extends Controller
         }
     }
 
-
-
     public function loginEleitor()
     {
         return view('login-eleitor');
     }
-
 
     public function handle($request, Closure $next)
     {
