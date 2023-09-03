208b92e48347079f0459337ee5f95aefd6b04ae8
Felipe Schottz <felipe.schottz@memora.com.br>
2023-09-03 11:30:24 -0300

    Corrigir indent

--- resources/views/login-eleitor.blade.php
+++ resources/views/login-eleitor.blade.php
@@ -23,59 +23,55 @@
         </div>
     </div>
     @if (session('error'))
-    <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="errorModalLabel">Erro</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    {{ session('error') }}
-                </div>
-            </div>
-        </div>
-    </div>
-    <script>
-        $(document).ready(function () {
-            $('#errorModal').modal('show');
-        });
-    </script>
-@endif
-
-@if (session('success'))
-    <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="successModalLabel">Insira o Código SMS</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <form action="{{ route('authenticate-eleitor') }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="codigo_sms">Código SMS</label>
-                            <input type="text" class="form-control row" name="codigo_sms" id="codigo_sms" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary">Entrar</button>
-                    </form>
+        <div class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="errorModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="errorModalLabel">Erro</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        {{ session('error') }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <script>
-        $(document).ready(function () {
-            $('#successModal').modal('show'); 
-        });
-    </script>
-@endif
+        <script>
+            $(document).ready(function () {
+                $('#errorModal').modal('show');
+            });
+        </script>
+    @endif
 
-    
-
-
+    @if (session('success'))
+        <div class="modal fade" id="successModal" tabindex="-1" role="dialog" aria-labelledby="successModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="successModalLabel">Insira o Código SMS</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('authenticate-eleitor') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="codigo_sms">Código SMS</label>
+                                <input type="text" class="form-control row" name="codigo_sms" id="codigo_sms" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Entrar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            $(document).ready(function () {
+                $('#successModal').modal('show'); 
+            });
+        </script>
+    @endif
 @endsection
