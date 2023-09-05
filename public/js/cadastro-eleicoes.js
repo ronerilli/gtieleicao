40319cb37c4e2fc2ca6eea3df2969d39e274b11a
Felipe Schottz <felipe.schottz@memora.com.br>
2023-09-05 14:05:24 -0300

    Add check data atual

--- public/js/cadastro-eleicoes.js
+++ public/js/cadastro-eleicoes.js
@@ -1,15 +1,22 @@
-// Capturando elementos do DOM
+var dataHoje = new Date();
 var dataInicioInput = document.getElementById("data_inicio");
 var dataFimInput = document.getElementById("data_fim");
 
-// Adicionando um ouvinte de evento para verificar quando a data de término é alterada
+dataInicioInput.addEventListener("change", function () {
+    var dataInicio = new Date(dataInicioInput.value);
+
+    if (dataInicio < dataHoje) {
+        openErrorModal("A data de início não pode ser anterior ao dia de hoje.");
+        dataFimInput.value = "";    
+    }
+});
+
 dataFimInput.addEventListener("change", function () {
     var dataInicio = new Date(dataInicioInput.value);
     var dataFim = new Date(dataFimInput.value);
 
-    // Verificando se a data de término é menor que a data de início
     if (dataFim < dataInicio) {
-        openErrorModal("A data de término não pode ser menor que a data de início.");
-        dataFimInput.value = ""; // Limpa o campo de data de término
+        openErrorModal("A data de término não pode ser anterior à data de início.");
+        dataFimInput.value = "";
     }
 });
