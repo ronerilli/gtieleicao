a076841d1bf9f33855c836f7698dcb215352989c
Felipe Schottz <felipe.schottz@memora.com.br>
2023-09-14 18:40:05 -0300

    add > delay check data

--- public/js/cadastro-eleicoes.js
+++ public/js/cadastro-eleicoes.js
@@ -1,22 +1,29 @@
 var dataHoje = new Date();
 var dataInicioInput = document.getElementById("data_inicio");
 var dataFimInput = document.getElementById("data_fim");
+var delay = 1000; 
 
-dataInicioInput.addEventListener("change", function () {
-    var dataInicio = new Date(dataInicioInput.value);
+var timeoutIdInicio, timeoutIdFim;
 
-    if (dataInicio < dataHoje) {
-        openErrorModal("A data de início não pode ser anterior ao dia de hoje.");
-        dataFimInput.value = "";    
-    }
+dataInicioInput.addEventListener("input", function () {
+    clearTimeout(timeoutIdInicio);
+    timeoutIdInicio = setTimeout(function () {
+        var dataInicio = new Date(dataInicioInput.value);
+
+        if (dataInicio < dataHoje) {
+            openErrorModal("A data de início não pode ser anterior ao dia de hoje.");
+        }
+    }, delay);
 });
 
-dataFimInput.addEventListener("change", function () {
-    var dataInicio = new Date(dataInicioInput.value);
-    var dataFim = new Date(dataFimInput.value);
+dataFimInput.addEventListener("input", function () {
+    clearTimeout(timeoutIdFim); 
+    timeoutIdFim = setTimeout(function () {
+        var dataInicio = new Date(dataInicioInput.value);
+        var dataFim = new Date(dataFimInput.value);
 
-    if (dataFim < dataInicio) {
-        openErrorModal("A data de término não pode ser anterior à data de início.");
-        dataFimInput.value = "";
-    }
+        if (dataFim < dataInicio) {
+            openErrorModal("A data de término não pode ser anterior à data de início.");
+        }
+    }, delay);
 });
